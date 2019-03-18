9bc5b99c4fbac8dc780e6ba5333a33d701379ee7
felixkorch <korch.felix@gmail.com>
2019-03-19 00:58:09 +0100

    Update GenericWindow.cpp

--- src/GenericWindow.cpp
+++ src/GenericWindow.cpp
@@ -67,6 +67,7 @@ namespace sgl
 		if (!glfwInit())
 			return -1;
 
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
