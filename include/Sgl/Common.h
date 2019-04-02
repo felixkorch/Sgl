46b845e5ed32874cd2215bcb2f15db2733fddc3c
felixkorch <korch.felix@gmail.com>
2019-04-02 20:46:20 +0200

    Update Common.h

--- include/Sgl/Common.h
+++ include/Sgl/Common.h
@@ -16,39 +16,8 @@
             std::terminate(); \
         } \
     } while (false)
-
+#else
+#define SglAssert(condition, message)
 #endif
 
 #define GLM_ENABLE_EXPERIMENTAL
-
-// Originally implemented by Stephan T. Lavavej
-namespace sgl {
-	template<class T> struct _Unique_if {
-		typedef std::unique_ptr<T> _Single_object;
-	};
-
-	template<class T> struct _Unique_if<T[]> {
-		typedef std::unique_ptr<T[]> _Unknown_bound;
-	};
-
-	template<class T, size_t N> struct _Unique_if<T[N]> {
-		typedef void _Known_bound;
-	};
-
-	template<class T, class... Args>
-	typename _Unique_if<T>::_Single_object
-		make_unique(Args&&... args) {
-		return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-	}
-
-	template<class T>
-	typename _Unique_if<T>::_Unknown_bound
-		make_unique(size_t n) {
-		typedef typename std::remove_extent<T>::type U;
-		return std::unique_ptr<T>(new U[n]());
-	}
-
-	template<class T, class... Args>
-	typename _Unique_if<T>::_Known_bound
-		make_unique(Args&&...) = delete;
-}
